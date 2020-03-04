4a3064083b2eb1c551f59027b4e26abc412622a0
reyusfa <yusuf.aryanto.1996@gmail.com>
2020-03-04 18:09:21 +0700

    change variable token value

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 
 const PrivateRoute = ({ component: Component, ...options }) => {
   const auth = useSelector(state => state.auth);
-  const token = auth.data ? auth.data : null;
+  const token = auth.data ? auth.data : undefined;
   
   return (
     <Route
